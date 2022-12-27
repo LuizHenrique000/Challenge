88e5aec9c7384bf4f0b1db737728739354964c49
LuizHenrique000 <luizlhra24@gmail.com>
2022-12-27 16:11:15 -0300

    Concluido

--- Biblioteca/src/App.jsx
+++ Biblioteca/src/App.jsx
@@ -1,8 +1,8 @@
-import { AppRoutes } from "./Routes";
 import Home from "./pages/home-page/home";
 import Favoritos from "./pages/favoritos-page/Favoritos";
 import { FavoritosStorage } from "./contexts/FavoritosContext";
 import { LivrosStorage } from "./contexts/LivrosContext";
+import { AppRoutes } from "./Routes";
 
 function App() {
   return (
